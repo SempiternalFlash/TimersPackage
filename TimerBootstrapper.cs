8684c4958ef732483016478a5bbde6f8ef518133
David Zábranský <zabranskydavid2@gmail.com>
2024-07-05 19:31:20 +0200

    Initial commit for Timers package for Unity

---
+++ TimerBootstrapper.cs
@@ -0,0 +1,63 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.LowLevel;
+using UnityEngine.PlayerLoop;
+
+namespace Timers
+{
+    internal static class TimerBootstrapper
+    {
+        private static PlayerLoopSystem _timerSystem;
+    
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+        internal static void Initialize()
+        {
+            var currentPlayerLoop = PlayerLoop.GetCurrentPlayerLoop();
+
+            if (!InsertTimerManager<Update>(ref currentPlayerLoop, 0))
+            {
+                Debug.LogWarning("Timers not initialized, unable to register TimerManager into the Update loop.");
+                return;
+            }
+        
+            PlayerLoop.SetPlayerLoop(currentPlayerLoop);
+            PlayerLoopUtils.PrintPlayerLoop(currentPlayerLoop);
+        
+#if UNITY_EDITOR
+            EditorApplication.playModeStateChanged -= OnPlayModeState;
+            EditorApplication.playModeStateChanged += OnPlayModeState;
+#endif
+
+            static void OnPlayModeState(PlayModeStateChange state)
+            {
+                if (state == PlayModeStateChange.ExitingPlayMode)
+                {
+                    PlayerLoopSystem currentPlayerLoop = PlayerLoop.GetCurrentPlayerLoop();
+                    RemoveTimerManager<Update>(ref currentPlayerLoop);
+                    PlayerLoop.SetPlayerLoop(currentPlayerLoop);
+                
+                    TimerManager.Clear();
+                }
+            }
+        }
+
+        private static void RemoveTimerManager<T>(ref PlayerLoopSystem loop)
+        {
+            PlayerLoopUtils.RemoveSystem<T>(ref loop, in _timerSystem);
+        }
+
+        private static bool InsertTimerManager<T>(ref PlayerLoopSystem loop, int index)
+        {
+            _timerSystem = new PlayerLoopSystem()
+            {
+                type = typeof(TimerManager),
+                updateDelegate = TimerManager.UpdateTimers,
+                subSystemList = null
+            };
+
+            return PlayerLoopUtils.InsertSystem<T>(ref loop, in _timerSystem, index);
+        }
+
+    
+    }
+}
